fc5f95b52053293c0844e7942abdb897e3722883
kochyan <lllalll7lblk@gmail.com>
2023-05-14 18:49:51 +0400

    add decryptor

---
+++ src/main/java/ru/sstu/ifbs/compoents/DemonstrateComponent.java
@@ -0,0 +1,25 @@
+package ru.sstu.ifbs.compoents;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Component
+public class DemonstrateComponent {
+    private final static Logger logger = Logger.getLogger(DemonstrateComponent.class.getName());
+
+    @Value("${example1}")
+    private String firstValue;
+    @Value("${example2}")
+    private String secondValue;
+
+    @PostConstruct
+    public void init() {
+        logger.log(Level.INFO, "firstValue: " + firstValue);
+        logger.log(Level.INFO, "secondValue: " + secondValue);
+    }
+}
